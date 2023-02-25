cf88c15ea3204d069f9c876589b86f891c8befaa
canish26 <112290448+canish26@users.noreply.github.com>
2023-02-24 20:57:21 -0800

    HW_02 submission
    Changes are done as per PR changes said and completed last part

--- scripts/Script-2.sql
+++ scripts/Script-2.sql
@@ -1,7 +1,10 @@
 use baseball;
-SELECT * FROM game LIMIT 0, 20;
+SELECT local_date  FROM game LIMIT 0, 20;
+-- Creating Tables to store the calculated averages
+-- Historic batting average for each player
+DROP TABLE IF EXISTS batt_avg_hist;
 
-create table batt_avg_hist as
+CREATE TABLE IF NOT EXISTS batt_avg_hist as
 SELECT 
     batter, 
     SUM(hit) AS total_hits, 
@@ -11,10 +14,15 @@ FROM
     batter_counts bc 
 GROUP BY 
     batter ;
+#ALTER TABLE batt_avg_hist ADD PRIMARY KEY (batter);
     
 SELECT * FROM batt_avg_hist ;
-   
-create table batt_avg_annual as
+
+-- Annual batting average for each player
+
+DROP TABLE IF EXISTS batt_avg_annual;
+
+CREATE TABLE IF NOT EXISTS batt_avg_annual as
 SELECT batter AS Batter,
       YEAR(game.local_date) AS For_Year,
        SUM(hit) AS total_hits, 
@@ -22,8 +30,54 @@ SELECT batter AS Batter,
     CAST(SUM(hit) AS FLOAT) / NULLIF(SUM(atBat), 0) AS batting_average
 FROM batter_counts AS bc
 INNER JOIN game ON bc.game_id = game.game_id
-GROUP BY Batter, For_Year
-ORDER BY Batter, For_Year;
+GROUP BY
+        Batter, For_Year
+ORDER BY 
+        Batter, For_Year;
+       
+ALTER TABLE batt_avg_annual ADD PRIMARY KEY (batter, For_Year);
 
 SELECT * FROM batt_avg_annual ;
 
+#############
+CREATE table IF NOT EXISTS game_bat_int as
+select bc.game_id, bc.batter, local_date, bc.hit, bc.atBat, g.local_date
+from batter_counts bc
+inner join game g on bc.game_id=g.game_id;
+
+SELECT * from game_bat_int limit 0,20;
+
+ALTER table game_bat_int add index batter_index(batter);
+
+ALTER table game_bat_int add index date_index(local_date);
+
+
+##############
+DROP TABLE temp_roll_avg_intermediate;
+-- create temp table to store data
+CREATE  TABLE temp_roll_avg_intermediate
+AS (
+  SELECT bc.batter, g.local_date, bc.hit, bc.atBat
+  FROM batter_counts AS bc
+  LEFT JOIN game AS g
+  ON bc.game_id = g.game_id
+);
+-- creating index
+CREATE INDEX idx_batter ON temp_roll_avg_intermediate(batter);
+CREATE INDEX idx_date ON temp_roll_avg_intermediate(local_date);
+CREATE INDEX idx_batter_date ON temp_roll_avg_intermediate(batter, local_date);
+
+DROP table tmp_rolling_avg;
+-- Rolling avg
+CREATE TEMPORARY TABLE temp_roll_avg
+AS (
+  SELECT a.batter, a.local_date, (SUM(b.hit) / NULLIF(SUM(b.atBat), 0)) AS rolling_avg
+  FROM temp_roll_avg_intermediate AS a
+  JOIN temp_roll_avg_intermediate AS b
+  ON a.batter = b.batter AND a.local_date > b.local_date AND b.local_date BETWEEN a.local_date - INTERVAL 100 DAY AND a.local_date
+  WHERE a.batter = 435623
+  GROUP BY a.batter, a.local_date
+);
+
+SELECT * FROM temp_roll_avg
+ORDER BY local_date DESC;
