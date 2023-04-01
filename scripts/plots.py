02ec61484c34b3b3e926f930bb66d62bfae3b0b5
canish26 <112290448+canish26@users.noreply.github.com>
2023-04-01 13:36:33 -0700

    updated plots

--- scripts/plots.py
+++ scripts/plots.py
@@ -16,50 +16,112 @@ class Plots:
         merged_df = pd.merge(bin_mean_df[[x, resp_col]], hist_df, on=x)
         merged_df["pop_prop"] = merged_df[y] / merged_df[y].sum()
         weighted_morp = (
-            merged_df["pop_prop"] * (merged_df[resp_col] - df[resp_col].mean()) ** 2
+                merged_df["pop_prop"] * (merged_df[resp_col] - df[resp_col].mean()) ** 2
         ).sum()
 
         return unweighted_morp, weighted_morp
 
     @staticmethod
     def morp_cat_resp_cat_pred(df, pred_col, pred_type, resp_col):
-        # Calculate bin means for given class
-        hist_df = df[pred_col].value_counts(sort=False).reset_index()
-        bin_mean_df = (
-            df.groupby(pred_col)[resp_col]
-            .mean()
-            .reset_index()
-            .rename(columns={resp_col: f"{resp_col}_mean"})
-        )
+        # Create a pivot table with the mean response for each category
+        pivot_table = pd.pivot_table(
+            df, values=resp_col, index=pred_col, aggfunc=np.mean
+        ).reset_index()
 
-        # Create bar chart with two y-axes
+        # Create a bar chart with two y-axes
         fig = make_subplots(specs=[[{"secondary_y": True}]])
+        # figure1
         fig.add_trace(
-            go.Bar(x=hist_df["index"], y=hist_df[pred_col], name=pred_col),
+            go.Bar(x=df[pred_col].value_counts().index, y=df[pred_col].value_counts().values, name=pred_col),
             secondary_y=False,
         )
-        # 1st graph
+        # figure2
         fig.add_trace(
             go.Scatter(
-                x=bin_mean_df[pred_col],
-                y=bin_mean_df[f"{resp_col}_mean"],
+                x=pivot_table[pred_col],
+                y=pivot_table[resp_col],
                 mode="lines+markers",
                 name=f"bin_avg_for_{resp_col}",
                 marker=dict(color="red"),
             ),
             secondary_y=True,
         )
-        # 2nd graph
+
+        # Add layout information
+        fig.update_layout(
+            title_text=f"Mean_response_plot_for_{pred_col}_and_{resp_col.lower()}",
+            yaxis=dict(title="Total Population", side="left"),
+            yaxis2=dict(
+                title="Response",
+                side="right",
+                overlaying="y",
+                tickmode="auto",
+            ),
+            xaxis=dict(title=f"{pred_col}_bins"),
+        )
+
+        # Save the plot to a file
+        file_name = f"{p.GLOBAL_PATH}/cat_{resp_col}_cat_{pred_col}.html"
+        fig.write_html(file=file_name, include_plotlyjs="cdn")
+
+        # Calculate MORP values
+        hist_counts = df[pred_col].value_counts(sort=False).values
+        weighted_morp = np.sum(hist_counts * np.abs(pivot_table[resp_col] - df[resp_col].mean()))
+        unweighted_morp = np.abs(pivot_table[resp_col].diff().mean())
+
+        # Return the result as a dictionary
+        return {
+            "col_name": pred_col,
+            "col_type": pred_type,
+            "weighted_morp": weighted_morp,
+            "unweighted_morp": unweighted_morp,
+            "Plot_link": file_name,
+        }
+
+    @staticmethod
+    def morp_cat_resp_cont_pred(df, pred_col, pred_type, resp_col):
+        # Define binning column name
+        bin_col = f"{pred_col}_bin"
+
+        # Bin the predictor variable and calculate mean response for each bin
+        df[bin_col] = pd.cut(df[pred_col], bins=10, right=True).apply(lambda x: x.mid)
+        bin_mean_df = df.groupby(bin_col)[resp_col].mean().reset_index()
+
+        # Create histogram of bins
+        hist_df = pd.DataFrame(df[bin_col].value_counts(sort=False))
+        hist_df.index.name = bin_col
+        hist_df.columns = ["Total Population"]
+
+        # Calculate MORP values
+        weighted_morp, unweighted_morp = Plots.weigh_and_unweigh_morp(
+            bin_mean_df, hist_df, df, bin_col, resp_col, resp_col
+        )
+
+        # Create plot with two y-axes
+        fig = make_subplots(specs=[[{"secondary_y": True}]])
+        fig.add_trace(
+            go.Bar(x=hist_df.index, y=hist_df["Total Population"], name="Total Population"),
+            secondary_y=False,
+        )
         fig.add_trace(
             go.Scatter(
-                x=bin_mean_df[pred_col],
-                y=[df[resp_col].mean()] * len(bin_mean_df),
-                mode="lines",
-                name=f"{pred_col}_avg",
+                x=bin_mean_df[bin_col],
+                y=bin_mean_df[resp_col],
+                mode="lines+markers",
+                name=f"bin_avg_for_{resp_col}",
+                marker=dict(color="red"),
             ),
             secondary_y=True,
         )
-        # update layout
+        fig.add_trace(
+            go.Scatter(
+                x=bin_mean_df[bin_col],
+                y=[df[resp_col].mean()] * len(bin_mean_df),
+                mode="lines",
+                name=f"{bin_col}_avg",
+            ),
+            secondary_y=True,
+        )
         fig.update_layout(
             title_text=f"Mean_response_plot_for_{pred_col}_and_{resp_col.lower()}",
             yaxis=dict(title="Total Population", side="left"),
@@ -73,13 +135,8 @@ class Plots:
         )
 
         # Save chart as HTML file
-        file_name = f"{p.GLOBAL_PATH}/cat_{resp_col}_cat_{pred_col}.html"
+        file_name = f"{p.GLOBAL_PATH}/cat_{resp_col}_cont_{pred_col}.html"
         fig.write_html(file=file_name, include_plotlyjs="cdn")
-
-        # Calculate MORP values
-        weighted_morp, unweighted_morp = Plots.weigh_and_unweigh_morp(
-            bin_mean_df, hist_df, df, pred_col, resp_col, resp_col
-        )
 
         # Return dictionary
         return {
@@ -91,85 +148,8 @@ class Plots:
         }
 
     @staticmethod
-    def morp_cat_resp_cont_pred(df, pred_col, pred_type, resp_col):
-        class_nm = "bool_true"
-        # input column_nm and class_nm
-        x = pred_col + "_bin"
-        y = resp_col
-
-        # creating bins for each column and calculating their average
-        bins = pd.cut(df[pred_col], bins=10, right=True)
-        df[x] = bins.mid
-        bin_mean_df = df.groupby(x)[y].mean().reset_index()
-
-        # creating the histogram of bins
-        hist_df = bins.value_counts().reset_index()
-        hist_df.columns = ["bin", x]
-
-        # calculating morp values
-        weighted_morp, unweighted_morp = Plots.weigh_and_unweigh_morp(
-            bin_mean_df, hist_df, df, x, y, resp_col
-        )
-
-        # creating the plot
-        fig_bar = make_subplots(specs=[[{"secondary_y": True}]])
-        fig_bar.add_trace(
-            go.Bar(x=hist_df["bin"], y=hist_df[x], name=pred_col), secondary_y=False
-        )
-
-        # adding the first graph
-        fig_bar.add_trace(
-            go.Scatter(
-                x=bin_mean_df[x],
-                y=bin_mean_df[y],
-                name="bin_avg_for_" + class_nm,
-                mode="lines+markers",
-                marker=dict(color="Red"),
-            ),
-            secondary_y=True,
-        )
-
-        # adding the second graph
-        fig_bar.add_trace(
-            go.Scatter(
-                x=bin_mean_df[x],
-                y=[df[y].mean()] * len(bin_mean_df[x]),
-                name=class_nm + "_avg",
-                mode="lines",
-            ),
-            secondary_y=True,
-        )
-
-        # updating the layout
-        fig_bar.update_layout(
-            title_text="Mean_resp_plot_for_"
-            + pred_col
-            + "_and_"
-            + (class_nm.lower()).replace("-", "_"),
-            yaxis=dict(title=dict(text="Total_population"), side="left"),
-            yaxis2=dict(
-                title=dict(text="Resp"), side="right", overlaying="y", tickmode="auto"
-            ),
-            xaxis=dict(title=dict(text=pred_col + "_bins")),
-        )
-
-        # saving the plot to an HTML file
-        file_name = (
-            p.GLOBAL_PATH + "/" + "cat_" + resp_col + "_cont_" + pred_col + ".html"
-        )
-        fig_bar.write_html(file=file_name, include_plotlyjs="cdn")
-
-        return {
-            "col_name": pred_col,
-            "col_type": pred_type,
-            "weighted_morp": weighted_morp,
-            "unweighted_morp": unweighted_morp,
-            "Plot_link": file_name,
-        }
-
-    @staticmethod
-    def morp_cont_resp_cat_pred(df, pred_colmn, pred_type, resp_col):
-        x = pred_colmn
+    def morp_cont_resp_cat_pred(df, pred_col, pred_type, resp_col):
+        x = pred_col
         y = resp_col
 
         # Find bin mean for given class
@@ -198,22 +178,22 @@ class Plots:
             go.Scatter(
                 x=bin_mean_df[x],
                 y=[df[y].mean()] * len(bin_mean_df),
-                name=f"{pred_colmn}_avg",
+                name=f"{pred_col}_avg",
                 mode="lines",
             ),
             secondary_y=True,
         )
         # updating layout
         fig.update_layout(
-            title_text=f"Mean_response_plot_for_{pred_colmn}_and_{resp_col.lower()}",
+            title_text=f"Mean_resp_plot_for_{pred_col}_and_{resp_col.lower()}",
             yaxis=dict(title="Total Population", side="left"),
             yaxis2=dict(
                 title="Response", side="right", overlaying="y", tickmode="auto"
             ),
-            xaxis=dict(title=f"{pred_colmn}_bins"),
+            xaxis=dict(title=f"{pred_col}_bins"),
         )
         # Save chart as HTML file
-        file_name = f"{p.GLOBAL_PATH}/cont_{resp_col}_cat_{pred_colmn}.html"
+        file_name = f"{p.GLOBAL_PATH}/cont_{resp_col}_cat_{pred_col}.html"
         fig.write_html(file=file_name, include_plotlyjs="cdn")
 
         # Calculate MORP values
@@ -222,7 +202,7 @@ class Plots:
         )
 
         return {
-            "col_name": pred_colmn,
+            "col_name": pred_col,
             "col_type": pred_type,
             "weighted_morp": weighted_morp,
             "unweighted_morp": unweighted_morp,
