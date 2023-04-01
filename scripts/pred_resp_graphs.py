aea7944eb3122506c7064330893c8208da593419
canish26 <112290448+canish26@users.noreply.github.com>
2023-04-01 09:54:12 -0700

    Update pred_resp_graphs.py
    minor changes

--- scripts/pred_resp_graphs.py
+++ scripts/pred_resp_graphs.py
@@ -1,27 +1,19 @@
 import path as p
-import plotly.figure_factory as ff
+import plotly.express as px
+# import plotly.figure_factory as ff
 import plotly.graph_objects as go
+import plotly.subplots as sp
 
 
 class PlotGraph:
     @staticmethod
     def cat_resp_cat_pred(data_set, pred_col, resp_col):
-        pivoted_data = (
-            data_set.groupby([resp_col, pred_col]).size().unstack(fill_value=0)
+        fig = px.imshow(
+            data_set.groupby([resp_col, pred_col]).size().unstack(fill_value=0),
+            x=pred_col,
+            y=resp_col,
+            color_continuous_scale="Blues",
         )
-
-        heatmap = go.Heatmap(
-            x=pivoted_data.columns,
-            y=pivoted_data.index,
-            z=pivoted_data.values,
-            colorscale="Blues",
-        )
-
-        # Define the layout of the plot
-        layout = go.Layout(title="Heatmap", xaxis_title=pred_col, yaxis_title=resp_col)
-
-        # Create the figure
-        fig = go.Figure(data=[heatmap], layout=layout)
 
         file_name = f"{p.GLOBAL_PATH}/cat_{resp_col}_cat_{pred_col}_heatmap.html"
 
@@ -30,44 +22,41 @@ class PlotGraph:
             file=file_name,
             include_plotlyjs="cdn",
         )
+
         return {"column_nm": pred_col, "plot_link_1": file_name, "plot_link_2": None}
 
     @staticmethod
     def cat_resp_cont_pred(data_set, pred_col, resp_col):
-        # Create histogram data
-        hist_data = []
-        group_labels = []
-        for label, group in data_set.groupby(resp_col):
-            hist_data.append(group[pred_col])
-            group_labels.append(str(label))
+        fig_1 = px.histogram(
+            data_set,
+            x=pred_col,
+            color=resp_col,
+            marginal="rug",
+            histnorm="density",
+            nbins=20,
+        )
 
-        # Create distribution plot with custom bin_size
-        fig_1 = ff.create_distplot(hist_data, group_labels)
         fig_1.update_layout(
             title=f"Categorical {resp_col} vs Continuous {pred_col}",
             xaxis_title=pred_col,
-            yaxis_title=resp_col,
+            yaxis_title="Density",
         )
+
         file_name_1 = f"{p.GLOBAL_PATH}/cat_{resp_col}_cont_{pred_col}_distplot.html"
         fig_1.write_html(file=file_name_1, include_plotlyjs="cdn")
 
-        # Create violin plot
-        fig_2 = go.Figure()
-        for label, hist in zip(group_labels, hist_data):
-            fig_2.add_trace(
-                go.Violin(
-                    x=[label] * len(hist),
-                    y=hist,
-                    name=label,
-                    box_visible=True,
-                    meanline_visible=True,
-                )
-            )
-        fig_2.update_layout(
+        fig_2 = px.violin(
+            data_set,
+            x=resp_col,
+            y=pred_col,
+            box=True,
+            points="all",
+            color=resp_col,
             title=f"Categorical {resp_col} vs Continuous {pred_col}",
-            xaxis_title=resp_col,
             yaxis_title=pred_col,
+            xaxis_title=resp_col,
         )
+
         file_name_2 = f"{p.GLOBAL_PATH}/cat_{resp_col}_cont_{pred_col}_violin_plot.html"
         fig_2.write_html(file=file_name_2, include_plotlyjs="cdn")
 
@@ -87,75 +76,79 @@ class PlotGraph:
         group_labels = data_set[pred_col].unique()
 
         # Create distribution plot with custom bin_size
-        fig_1 = ff.create_distplot(hist_data, group_labels, bin_size=0.2)
-        fig_1.update_layout(
-            title=f"Continuous {resp_col} vs Categorical {pred_col}",
-            xaxis_title=resp_col,
-            yaxis_title="Distribution",
-        )
-        file_name_1 = f"{p.GLOBAL_PATH}/cont_{resp_col}_cat_{pred_col}_distplot.html"
-        fig_1.write_html(file=file_name_1, include_plotlyjs="cdn")
+        dist_figs = []
+        for i, hist in enumerate(hist_data):
+            fig = go.Figure()
+            fig.add_trace(go.Histogram(x=hist, nbinsx=50, name=group_labels[i]))
+            fig.update_layout(
+                title=f"{group_labels[i]} Distribution of {resp_col}",
+                xaxis_title=resp_col,
+                yaxis_title="Frequency",
+                height=400,
+                width=400,
+            )
+            dist_figs.append(fig)
 
         # Create violin plot
-        fig_2 = go.Figure()
-        for hist, label in zip(hist_data, group_labels):
-            fig_2.add_trace(
+        violin_figs = []
+        for i, hist in enumerate(hist_data):
+            fig = go.Figure()
+            fig.add_trace(
                 go.Violin(
-                    x=[label] * len(data_set),
+                    x=[group_labels[i]] * len(hist),
                     y=hist,
-                    name=label,
+                    name=group_labels[i],
                     box_visible=True,
                     meanline_visible=True,
                 )
             )
-        fig_2.update_layout(
-            title=f"Continuous {resp_col} vs Categorical {pred_col}",
-            xaxis_title="Groupings",
-            yaxis_title=resp_col,
+            fig.update_layout(
+                title=f"{group_labels[i]} {resp_col} vs {pred_col}",
+                xaxis_title=pred_col,
+                yaxis_title=resp_col,
+                height=400,
+                width=400,
+            )
+            violin_figs.append(fig)
+
+        # Combine subplots
+        fig = sp.make_subplots(
+            rows=2, cols=len(group_labels), subplot_titles=tuple(group_labels)
         )
-        file_name_2 = f"{p.GLOBAL_PATH}/cont_{resp_col}_cat_{pred_col}_violin_plot.html"
-        fig_2.write_html(file=file_name_2, include_plotlyjs="cdn")
+
+        for i, subplot in enumerate(dist_figs):
+            fig.add_trace(subplot["data"][0], row=1, col=i + 1)
+        for i, subplot in enumerate(violin_figs):
+            fig.add_trace(subplot["data"][0], row=2, col=i + 1)
+
+        fig.update_layout(
+            title=f"Continuous {resp_col} vs Categorical {pred_col}",
+            height=800,
+            width=1000,
+        )
+
+        file_name = f"{p.GLOBAL_PATH}/cont_{resp_col}_cat_{pred_col}_subplots.html"
+        fig.write_html(file=file_name, include_plotlyjs="cdn")
 
         return {
             "column_nm": pred_col,
-            "plot_link_1": file_name_1,
-            "plot_link_2": file_name_2,
+            "plot_link": file_name,
         }
 
     @staticmethod
     def cont_resp_cont_pred(data_set, pred_col, resp_col):
-        # Create the scatter plot using plotly.graph_objects
-        fig = go.Figure(
-            data=go.Scatter(x=data_set[pred_col], y=data_set[resp_col], mode="markers")
-        )
-
-        # Add a trendline to the scatter plot
-        fig.add_trace(
-            go.Scatter(
-                x=data_set[pred_col],
-                y=fig["data"][0]["line"]["y"],
-                mode="lines",
-                line=dict(color="red"),
-                name="trendline",
-            )
-        )
+        # Create scatter plot with trendline
+        fig = px.scatter(data_set, x=pred_col, y=resp_col, trendline="ols")
 
         # Add layout information
         fig.update_layout(
-            title="Continuous " + resp_col + " vs " + " Continuous " + pred_col,
+            title=f"Continuous {resp_col} vs Continuous {pred_col}",
             xaxis_title=pred_col,
             yaxis_title=resp_col,
         )
 
         # Save the plot to a file
-        file_name = (
-            p.GLOBAL_PATH
-            + "/cont_"
-            + resp_col
-            + "_cont_"
-            + pred_col
-            + "_scatter_plot.html"
-        )
+        file_name = f"{p.GLOBAL_PATH}/cont_{resp_col}_cont_{pred_col}_scatter_plot.html"
         fig.write_html(file=file_name, include_plotlyjs="cdn")
 
         # Return the result as a dictionary
