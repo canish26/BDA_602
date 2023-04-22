4b3550503b6cd1ffdc644ce5ed67a1d485d8d5d8
canish26 <112290448+canish26@users.noreply.github.com>
2023-04-22 06:37:02 -0700

    sql queries update

--- scripts/Script-4.sql
+++ scripts/Script-4.sql
@@ -1,261 +1,72 @@
-/*Batting Average (BA): */
+USE baseball;
+
+CREATE TEMPORARY TABLE IF NOT EXISTS start_pitch_stats ENGINE=MEMORY AS
 SELECT
-  game.home_team_id AS team_id,
-  'Home' AS home_away,
-  SUM(batter_counts.Hit) / SUM(batter_counts.atBat) AS batter_counts_avg
-FROM
-  game
-  JOIN batter_counts ON game.game_id = batter_counts.game_id AND game.home_team_id = batter_counts.team_id
-GROUP BY
-  game.home_team_id, 'Home'
+    pc.game_id AS GameID
+    , DATE(g.local_date) AS GameDate
+    , pc.pitcher AS PitcherID
+    , pc.homeTeam AS HomeTeam
+    , SUM(pc.plateApperance) AS BattersFaced
+    , SUM(pc.outsPlayed/3) AS InningsPitched
+    , SUM(pc.Walk)/SUM(pc.outsPlayed)*27 AS BB9
+    , SUM(pc.Hit)/SUM(pc.outsPlayed)*27 AS H9
+    , SUM(pc.Home_Run)/SUM(pc.outsPlayed)*27 AS HR9
+    , SUM(pc.Strikeout)/(SUM(pc.outsPlayed)*3) * 27 AS SO9
+    , SUM(pc.Strikeout)/SUM(pc.pitchesThrown) AS StrikeoutPercent
+    , 0.89*(1.255*(pc.Hit-pc.Home_Run) + 4*pc.Home_Run)
+        + 0.56*(pc.Walk+pc.Hit_By_Pitch-pc.Intent_Walk) AS PowerPitchingFactor
+    , IF(SUM(pc.outsPlayed) = 0, 0, 3 + ((13*SUM(pc.Home_Run)+3*(SUM(pc.Walk+pc.Hit_By_Pitch)-2*SUM(pc.Strikeout)))/SUM(pc.outsPlayed))) AS DICECoefficient
+    , IF(SUM(pc.outsPlayed) = 0, 0, (SUM(pc.Hit)+SUM(pc.Walk))/SUM(pc.outsPlayed)) AS WHIP
+    , IF(SUM(pc.outsPlayed)+SUM(pc.plateApperance) = 0, 0, 9*((SUM(pc.Hit)+SUM(pc.Walk)+SUM(pc.Hit_By_Pitch)-SUM(pc.Home_Run))*SUM(pc.pitchesThrown))/(SUM(pc.plateApperance)*SUM(pc.outsPlayed))-0.56) AS CERA
+    , SUM(pc.Balk)/SUM(pc.outsPlayed)*27 AS BK9
+    , SUM(pc.Hit_By_Pitch)/SUM(pc.outsPlayed)*27 AS HBP9
+    , SUM(pc.Wild_Pitch)/SUM(pc.outsPlayed)*27 AS WP9
+    , SUM(pc.Intent_Walk)/SUM(pc.plateApperance)*100 AS IBBPercent
+    , (SUM(pc.Hit) + SUM(pc.Double) + 2*SUM(pc.Triple) + 3*SUM(pc.Home_Run)) / SUM(pc.plateApperance) AS AVG
+    , SUM(pc.Hit)/(SUM(pc.Hit) + SUM(pc.Strikeout)) AS ContactPercent
+    , bs.winner_home_or_away AS WinningTeam
+    , IF(bs.winner_home_or_away='H', 1, 0) AS HomeTeamWins
+FROM game g
+    JOIN pitcher_counts pc ON g.game_id = pc.game_id
+    JOIN boxscore bs ON g.game_id = bs.game_id
+WHERE pc.startingPitcher = 1
+GROUP BY GameID, PitcherID
+ORDER BY GameID;
 
-UNION ALL
 
+ALTER TABLE start_pitch_stats ADD PRIMARY KEY (game_id, pitcher);
+
+CREATE TEMPORARY TABLE IF NOT EXISTS roll_start_pitch_stats ENGINE=MEMORY AS
 SELECT
-  game.away_team_id AS team_id,
-  'Away' AS home_away,
-  SUM(batter_counts.Hit) / SUM(batter_counts.atBat) AS batter_counts_avg
-FROM
-  game
-  JOIN batter_counts ON game.game_id = batter_counts.game_id AND game.away_team_id = batter_counts.team_id
-GROUP BY
-  game.away_team_id, 'Away';
-  
-/*On-Base Percentage (OBP):  */
- 
- SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Home' AS home_away,
-    ROUND((SUM(batter_counts.Hit)+SUM(batter_counts.Walk)+SUM(batter_counts.Hit_By_Pitch))/
-          (SUM(batter_counts.atBat)+SUM(batter_counts.Walk)+SUM(batter_counts.Hit_By_Pitch)+SUM(batter_counts.Sac_Fly)),3) AS home_obp,
-    NULL AS away_obp
-FROM game 
-JOIN batter_counts ON game.game_id = batter_counts.game_id AND game.home_team_id = batter_counts.team_id
-GROUP BY game.game_id, game.home_team_id
-
-UNION ALL
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Away' AS home_away,
-    NULL AS home_obp,
-    ROUND((SUM(batter_counts.Hit)+SUM(batter_counts.Walk)+SUM(batter_counts.Hit_By_Pitch))/
-          (SUM(batter_counts.atBat)+SUM(batter_counts.Walk)+SUM(batter_counts.Hit_By_Pitch)+SUM(batter_counts.Sac_Fly)),3) AS away_obp
-FROM game 
-JOIN batter_counts ON game.game_id = batter_counts.game_id AND game.away_team_id = batter_counts.team_id
-GROUP BY game.game_id, game.away_team_id;
+    pc.game_id AS GameID,
+    DATE(g.local_date) AS GameDate,
+    pc.pitcher AS PitcherID,
+    pc.homeTeam AS HomeTeam,
+    SUM(pc.plateApperance) AS BattersFaced,
+    SUM(pc.outsPlayed/3) AS InningsPitched,
+    SUM(pc.Walk)/SUM(pc.outsPlayed)*27 AS BB9,
+    SUM(pc.Hit)/SUM(pc.outsPlayed)*27 AS H9,
+    SUM(pc.Home_Run)/SUM(pc.outsPlayed)*27 AS HR9,
+    SUM(pc.Strikeout)/(SUM(pc.outsPlayed)*3) * 27 AS SO9,
+    SUM(pc.Strikeout)/SUM(pc.plateApperance) AS StrikeoutPercent,
+    0.89*(1.255*(pc.Hit-pc.Home_Run) + 4*pc.Home_Run) + 0.56*(pc.Walk+pc.Hit_By_Pitch-pc.Intent_Walk) AS PowerPitchingFactor,
+    IF(SUM(pc.outsPlayed) = 0, 0, (13*SUM(pc.Home_Run) + 3*(SUM(pc.Walk) + SUM(pc.Hit_By_Pitch)) - 2*SUM(pc.Strikeout)) / SUM(pc.outsPlayed) + 3) AS DICECoefficient,
+    IF(SUM(pc.outsPlayed) = 0, 0, (SUM(pc.Walk) + SUM(pc.Hit)) / SUM(pc.outsPlayed)) AS WHIP,
+    IF(SUM(pc.pitchesThrown) = 0, 0, (SUM(pc.Hit) + SUM(pc.Walk) + SUM(pc.Hit_By_Pitch)) * (0.89*(1.255*(pc.Hit-pc.Home_Run) + 4*pc.Home_Run) + 0.56*(pc.Walk+pc.Hit_By_Pitch-pc.Intent_Walk)) / (SUM(pc.pitchesThrown) * SUM(pc.outsPlayed)) * 27 - 0.56) AS CERA,
+    SUM(pc.Balk)/SUM(pc.outsPlayed)*27 AS BK9,
+    SUM(pc.Hit_By_Pitch)/SUM(pc.outsPlayed)*27 AS HBP9,
+    SUM(pc.Wild_Pitch)/SUM(pc.outsPlayed)*27 AS WP9,
+    SUM(pc.Intent_Walk)/SUM(pc.plateApperance)*100 AS IBBPercent,
+    (SUM(pc.Hit) + SUM(pc.Double) + 2*SUM(pc.Triple) + 3*SUM(pc.Home_Run)) / SUM(pc.plateApperance) AS AVG,
+    SUM(pc.Hit)/(SUM(pc.Hit) + SUM(pc.Strikeout)) AS ContactPercent,
+    bs.winner_home_or_away AS WinningTeam,
+    IF(bs.winner_home_or_away='H', 1, 0) AS HomeTeamWins
+FROM game g
+    JOIN pitcher_counts pc ON g.game_id = pc.game_id AND pc.startingPitcher = 1
+    JOIN boxscore bs ON g.game_id = bs.game_id
+GROUP BY GameID, PitcherID
+ORDER BY GameID;
 
 
-/*Slugging Percentage (SLG):*/
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Home' AS home_away,
-    ROUND((SUM(batter_counts.Single) + 2 * SUM(batter_counts.Double) + 3 * SUM(batter_counts.Triple) + 4 * SUM(batter_counts.Home_Run)) / SUM(batter_counts.atBat),3) AS home_slg,
-    NULL AS away_slg
-FROM game 
-JOIN batter_counts ON game.game_id = batter_counts.game_id AND game.home_team_id = batter_counts.team_id
-GROUP BY game.game_id, game.home_team_id
 
-UNION ALL
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Away' AS home_away,
-    NULL AS home_slg,
-    ROUND((SUM(batter_counts.Single) + 2 * SUM(batter_counts.Double) + 3 * SUM(batter_counts.Triple) + 4 * SUM(batter_counts.Home_Run)) / SUM(batter_counts.atBat),3) AS away_slg
-FROM game 
-JOIN batter_counts ON game.game_id = batter_counts.game_id AND game.away_team_id = batter_counts.team_id
-GROUP BY game.game_id, game.away_team_id;
-
-/*On-Base Plus Slugging (OPS)*/
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Home' AS home_away,
-    ROUND(((SUM(batter_counts.Hit) + SUM(batter_counts.Walk) + SUM(batter_counts.Hit_By_Pitch)) / (SUM(batter_counts.atBat) + SUM(batter_counts.Walk) + SUM(batter_counts.Hit_By_Pitch) + SUM(batter_counts.Sac_Fly))) 
-    + ((SUM(batter_counts.Single) + 2 * SUM(batter_counts.Double) + 3 * SUM(batter_counts.Triple) + 4 * SUM(batter_counts.Home_Run)) / SUM(batter_counts.atBat)),3) AS home_ops,
-    NULL AS away_ops
-FROM game 
-JOIN batter_counts ON game.game_id = batter_counts.game_id AND game.home_team_id = batter_counts.team_id
-GROUP BY game.game_id, game.home_team_id
-
-UNION ALL
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Away' AS home_away,
-    NULL AS home_ops,
-    ROUND(((SUM(batter_counts.Hit) + SUM(batter_counts.Walk) + SUM(batter_counts.Hit_By_Pitch)) / (SUM(batter_counts.atBat) + SUM(batter_counts.Walk) + SUM(batter_counts.Hit_By_Pitch) + SUM(batter_counts.Sac_Fly))) 
-    + ((SUM(batter_counts.Single) + 2 * SUM(batter_counts.Double) + 3 * SUM(batter_counts.Triple) + 4 * SUM(batter_counts.Home_Run)) / SUM(batter_counts.atBat)),3) AS away_ops
-FROM game 
-JOIN batter_counts ON game.game_id = batter_counts.game_id AND game.away_team_id = batter_counts.team_id
-GROUP BY game.game_id, game.away_team_id;
-
-/*Runs Created (RC):*/
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Home' AS home_away,
-    ROUND((SUM(batter_counts.Hit) + SUM(batter_counts.Walk)) * (SUM(batter_counts.Hit) + SUM(batter_counts.Double) * 2 + SUM(batter_counts.Triple) * 3 + SUM(batter_counts.Home_Run) * 4) / (SUM(batter_counts.atBat) + SUM(batter_counts.Walk)), 3) AS home_rc,
-    NULL AS away_rc
-FROM game 
-JOIN batter_counts ON game.game_id = batter_counts.game_id AND game.home_team_id = batter_counts.team_id
-GROUP BY game.game_id, game.home_team_id
-
-UNION ALL
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Away' AS home_away,
-    NULL AS home_rc,
-    ROUND((SUM(batter_counts.Hit) + SUM(batter_counts.Walk)) * (SUM(batter_counts.Hit) + SUM(batter_counts.Double) * 2 + SUM(batter_counts.Triple) * 3 + SUM(batter_counts.Home_Run) * 4) / (SUM(batter_counts.atBat) + SUM(batter_counts.Walk)), 3) AS away_rc
-FROM game 
-JOIN batter_counts ON game.game_id = batter_counts.game_id AND game.away_team_id = batter_counts.team_id
-GROUP BY game.game_id, game.away_team_id;
-
-
-/*Strikeout-to-Walk Ratio (K/BB)*/
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Home' AS home_away,
-    ROUND(SUM(pitcher_counts.Strikeout) / SUM(pitcher_counts.Walk), 2) AS home_k_bb_ratio,
-    NULL AS away_k_bb_ratio
-FROM game 
-JOIN pitcher_counts ON game.game_id = pitcher_counts.game_id AND game.home_team_id = pitcher_counts.team_id
-GROUP BY game.game_id, game.home_team_id
-
-UNION ALL
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Away' AS home_away,
-    NULL AS home_k_bb_ratio,
-    ROUND(SUM(pitcher_counts.Strikeout) / SUM(pitcher_counts.Walk), 2) AS away_k_bb_ratio
-FROM game 
-JOIN pitcher_counts ON game.game_id = pitcher_counts.game_id AND game.away_team_id = pitcher_counts.team_id
-GROUP BY game.game_id, game.away_team_id;
-
-/*WHIP (Walks plus Hits per Inning Pitched)*/
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Home' AS home_away,
-    ROUND((SUM(pitcher_counts.Walk) + SUM(pitcher_counts.Hit)) / (SUM(pitcher_counts.Strikeout) / 3), 3) AS home_whip,
-    NULL AS away_whip
-FROM game 
-JOIN pitcher_counts ON game.game_id = pitcher_counts.game_id AND game.home_team_id = pitcher_counts.team_id
-GROUP BY game.game_id, game.home_team_id
-
-UNION ALL
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Away' AS home_away,
-    NULL AS home_whip,
-    ROUND((SUM(pitcher_counts.Walk) + SUM(pitcher_counts.Hit)) / (SUM(pitcher_counts.Strikeout) / 3), 3) AS away_whip
-FROM game 
-JOIN pitcher_counts ON game.game_id = pitcher_counts.game_id AND game.away_team_id = pitcher_counts.team_id
-GROUP BY game.game_id, game.away_team_id;
-
-
-/*CERA (Component ERA)*/
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Home' AS home_away,
-    ROUND((13 * SUM(pitcher_counts.Home_Run) + 3 * (SUM(pitcher_counts.Walk) + SUM(pitcher_counts.Hit_By_Pitch)) - 2 * SUM(pitcher_counts.Strikeout)) / SUM(pitcher_counts.pitchesThrown) * 27, 2) AS home_cera,
-    NULL AS away_cera
-FROM game 
-JOIN pitcher_counts ON game.game_id = pitcher_counts.game_id AND game.home_team_id = pitcher_counts.team_id
-GROUP BY game.game_id, game.home_team_id
-
-UNION ALL
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Away' AS home_away,
-    NULL AS home_cera,
-    ROUND((13 * SUM(pitcher_counts.Home_Run) + 3 * (SUM(pitcher_counts.Walk) + SUM(pitcher_counts.Hit_By_Pitch)) - 2 * SUM(pitcher_counts.Strikeout)) / SUM(pitcher_counts.pitchesThrown) * 27, 2) AS away_cera
-FROM game 
-JOIN pitcher_counts ON game.game_id = pitcher_counts.game_id AND game.away_team_id = pitcher_counts.team_id
-GROUP BY game.game_id, game.away_team_id;
-
-/*Strikeouts per Nine Innings (K/9)*/
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Home' AS home_away,
-    ROUND((SUM(pitcher_counts.Strikeout)/SUM(pitcher_counts.pitchesThrown)*9), 1) AS home_k_per_9,
-    NULL AS away_k_per_9
-FROM game 
-JOIN pitcher_counts ON game.game_id = pitcher_counts.game_id AND game.home_team_id = pitcher_counts.team_id
-GROUP BY game.game_id, game.home_team_id
-
-UNION ALL
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Away' AS home_away,
-    NULL AS home_k_per_9,
-    ROUND((SUM(pitcher_counts.Strikeout)/SUM(pitcher_counts.pitchesThrown)*9), 1) AS away_k_per_9
-FROM game 
-JOIN pitcher_counts ON game.game_id = pitcher_counts.game_id AND game.away_team_id = pitcher_counts.team_id
-GROUP BY game.game_id, game.away_team_id;
-
-/*BABIP – Batting average on balls in play*/
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Home' AS home_away,
-    ROUND((SUM(batter_counts.Hit)-SUM(batter_counts.Home_Run))/
-          (SUM(batter_counts.atBat)-SUM(batter_counts.Strikeout)-SUM(batter_counts.Home_Run)+SUM(batter_counts.Sac_Fly)),3) AS home_babip,
-    NULL AS away_babip
-FROM game 
-JOIN batter_counts ON game.game_id = batter_counts.game_id AND game.home_team_id = batter_counts.team_id
-GROUP BY game.game_id, game.home_team_id
-
-UNION ALL
-
-SELECT 
-    game.game_id, 
-    game.home_team_id, 
-    game.away_team_id,
-    'Away' AS home_away,
-    NULL AS home_babip,
-    ROUND((SUM(batter_counts.Hit)-SUM(batter_counts.Home_Run))/
-          (SUM(batter_counts.atBat)-SUM(batter_counts.Strikeout)-SUM(batter_counts.Home_Run)+SUM(batter_counts.Sac_Fly)),3) AS away_babip
-FROM game 
-JOIN batter_counts ON game.game_id = batter_counts.game_id AND game.away_team_id = batter_counts.team_id
-GROUP BY game.game_id, game.away_team_id;
+ALTER TABLE roll_start_pitch_stats ADD PRIMARY KEY (game_id, pitcher);
